b868ec52934e1dcfafe5268767a3516c4ccf7f4f
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-06-04 20:11:32 +0300

    burgerMenu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,13 @@
 import './header.scss';
 
-const btn = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger');
 
 const toggleBurger = e => {
   if (!e.currentTarget) return;
   console.log(e.target.nodeName);
   if (e.currentTarget) {
-    btn.classList.toggle('is-active');
+    hamburger.classList.toggle('is-active');
   }
 };
-btn.addEventListener('click', toggleBurger);
+
+hamburger.addEventListener('click', toggleBurger);
