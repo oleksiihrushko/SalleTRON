a909a0f149c803e115cb7d23e59a8f6251994d82
amie-affection <amie.affection@gmail.com>
2020-06-05 18:08:12 +0300

    modified files

---
+++ src/components/AuthForm/authForm.js
@@ -0,0 +1,28 @@
+const checkForm = document.querySelector('.js-form');
+const errorEmail = document.querySelector('.errorEmail');
+const errorPassword = document.querySelector('.errorPassword');
+
+checkForm.addEventListener('submit', checkValue);
+
+function checkValue(e) {
+  e.preventDefault();
+  const valueEmail = e.currentTarget.elements.email.value;
+  // console.log(valueEmail);
+  if (!(valueEmail.includes('@') && valueEmail.includes('.'))) {
+    errorEmail.textContent = 'Incorrect e-mail!';
+    return;
+  }
+
+  if (valueEmail.length < 5) {
+    errorEmail.textContent = 'Incorrect e-mail!';
+    return;
+  }
+
+  const valuePassword = e.currentTarget.elements.password.value;
+  console.log(valuePassword);
+
+  if (valuePassword.length < 6) {
+    errorPassword.textContent = 'Your password must be more than 5 symbols!';
+  }
+  e.currentTarget.reset();
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/styles.scss';
 
 import './components/mainPage/mainPage';
+
+import './components/AuthForm/authForm';
