bca717f6d2fc0745df81bb00df9269d3852d8613
ISvirko <ISvirko@yahoo.com>
2020-06-06 11:06:11 +0300

    autoplay added

--- src/components/categoryListItem/categoryListItem.js
+++ src/components/categoryListItem/categoryListItem.js
@@ -1,6 +1,9 @@
 import '@glidejs/glide/dist/css/glide.core.min.css';
 import '@glidejs/glide/dist/css/glide.theme.css';
 import Glide from '@glidejs/glide';
+import {
+  autoplay
+} from '@glidejs/glide/dist/glide.modular.esm'
 
 import apiServices from '../../services/api';
 import {
@@ -8,24 +11,26 @@ import {
   getLiMarkup
 } from './categoryListItemMarkup';
 
-import './categoryListItem.scss'
+import './categoryListItem.scss';
 
 ///////////////////////////////////////////////////////////////
 const categoryList = document.querySelector('.categoryList');
-categoryList.addEventListener('click', (e) => {
+categoryList.addEventListener('click', e => {
   if (e.target.nodeName === 'IMG' || e.target.nodeName === 'P') {
     console.log(e.target.dataset.id); // заменить консоль лог функцией
   }
-})
+});
 ////////////////////////////////////////////////////////////////
 
-export async function getCategory(category) {
+export async function getCategoryListItem(category) {
   const glide = new Glide('.glide', {
     type: 'carousel',
     perView: 4,
     dots: '#dots',
+    autoplay: 6000,
     breakpoints: {
       1279: {
+        gap: 26,
         perView: 2,
       },
       767: {
@@ -34,15 +39,17 @@ export async function getCategory(category) {
     },
   });
 
-  const categoryItems = await apiServices
-    .getProductsByCategory(category);
+  const categoryItems = await apiServices.getProductsByCategory(category);
 
   const itemMarkup = categoryItems.reduce((acc, item) => {
     acc += getItemMarkup(item);
-    return acc
+    return acc;
   }, '');
 
-  categoryList.insertAdjacentHTML('afterbegin', getLiMarkup(category, itemMarkup, categoryItems));
+  categoryList.insertAdjacentHTML(
+    'afterbegin',
+    getLiMarkup(category, itemMarkup, categoryItems),
+  );
 
   glide.mount();
 
@@ -50,38 +57,50 @@ export async function getCategory(category) {
   seeAllButton.addEventListener('click', showAllProducts);
 
   function showAllProducts() {
-    const categoryContainer = document.querySelector('.replacableContainer')
-    categoryContainer.innerHTML = innerMarkup(categoryItems, 0, products.visible);
+    const categoryContainer = document.querySelector('.replacableContainer');
+    categoryContainer.innerHTML = innerMarkup(
+      categoryItems,
+      0,
+      products.visible,
+    );
 
     const loadMoreBtn = document.querySelector('.loadMoreBtn');
-    loadMoreBtn.addEventListener('click', showMoreProducts)
-    loadMoreBtn.classList.remove('hidden')
+    loadMoreBtn.addEventListener('click', showMoreProducts);
+    loadMoreBtn.classList.remove('hidden');
+
+    endOfCategoryHandler(categoryItems, loadMoreBtn)
 
     function showMoreProducts() {
-      if (products.visible === categoryItems.length) {
-        loadMoreBtn.disabled = true;
-        loadMoreBtn.classList.replace('button', 'button--inactive');
-        loadMoreBtn.querySelector('span').textContent = 'No more hereg'
-        return;
-      }
-
-      loadMoreBtn.classList.add('button--loading')
-      const addedProducts = innerMarkup(categoryItems, products.visible, (products.visible + 12));
+      loadMoreBtn.classList.add('button--loading');
+      const addedProducts = innerMarkup(
+        categoryItems,
+        products.visible,
+        products.visible + 12,
+      );
       products.visible += 12;
 
       categoryContainer.insertAdjacentHTML('beforeend', addedProducts);
-      loadMoreBtn.classList.remove('button--loading')
+      loadMoreBtn.classList.remove('button--loading');
     }
   }
 }
 
 const products = {
   visible: 12,
-}
+};
 
 function innerMarkup(categoryItems, start, end) {
   return `
     ${categoryItems.slice(start, end).reduce((acc, item) => {
-          return acc += getItemMarkup(item)
-        }, '')}`
+      return (acc += getItemMarkup(item));
+    }, '')}`;
+}
+
+function endOfCategoryHandler(categoryItems, loadMoreBtn) {
+  if (products.visible >= categoryItems.length) {
+    loadMoreBtn.disabled = true;
+    loadMoreBtn.classList.replace('button', 'button--inactive');
+    loadMoreBtn.querySelector('span').textContent = 'No more here';
+    return;
+  }
 }
