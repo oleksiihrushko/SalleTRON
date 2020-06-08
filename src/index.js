9b21db90bc83ca3823ecf43535e3b12be25d1a0f
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-06-08 18:47:17 +0300

    hs

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import './css/styles.scss';
 import './components/mainPage/mainPage';
-<<<<<<< HEAD
 import './components/ItemModal/ItemModal';
 import './components/navigationMenu/navigationMenu';
 import './services/api';
 import './components/header/header';
-
-import './components/authentication/authServises'
-<<<<<<< HEAD
-=======
-
+import './components/authentication/authServises';
 import './components/AuthForm/authOpenModal';
->>>>>>> AuthForm
-=======
-
 import './components/AuthForm/authOpenModal';
->>>>>>> authentication
