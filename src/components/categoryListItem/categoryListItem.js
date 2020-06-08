aa9a54fda62e3956273b81a3c7d699206a284380
hrushkooleksii <hrushkooleksii@gmail.com>
2020-06-08 21:09:28 +0300

    remerge categoryList

--- src/components/categoryListItem/categoryListItem.js
+++ src/components/categoryListItem/categoryListItem.js
@@ -3,22 +3,18 @@ import '@glidejs/glide/dist/css/glide.theme.css';
 import Glide from '@glidejs/glide';
 
 import apiServices from '../../services/api';
-import {
-  getItemMarkup,
-  getLiMarkup
-} from './categoryListItemMarkup';
+import { getItemMarkup, getLiMarkup } from './categoryListItemMarkup';
+import openItemModal from '../ItemModal/ItemModal';
 
 import './categoryListItem.scss';
 
-///////////////////////////////////////////////////////////////
 const categoryList = document.querySelector('.categoryList');
 
 categoryList.addEventListener('click', e => {
   if (e.target.nodeName === 'IMG' || e.target.nodeName === 'P') {
-    console.log(e.target.dataset.id); // заменить консоль лог функцией
+    openItemModal(e.target.dataset.id);
   }
 });
-////////////////////////////////////////////////////////////////
 
 export async function getCategoryListItem(category) {
   const categoryItems = await apiServices.getProductsByCategory(category);
@@ -34,7 +30,6 @@ export async function getCategoryListItem(category) {
 
   const categoryItem = document.createElement('div');
   categoryList.append(categoryItem);
-
 
   function getSlider() {
     categoryItem.innerHTML = getLiMarkup(category, itemMarkup, categoryItems);
@@ -68,7 +63,9 @@ export async function getCategoryListItem(category) {
   seeAllButton.addEventListener('click', seeAllProducts);
 
   function seeAllProducts(e) {
-    const categoryContainer = document.querySelector(`[data-replace=${e.target.dataset.btnreplace}]`);
+    const categoryContainer = document.querySelector(
+      `[data-replace=${e.target.dataset.btnreplace}]`,
+    );
 
     categoryContainer.innerHTML = innerMarkup(
       categoryItems,
@@ -76,7 +73,9 @@ export async function getCategoryListItem(category) {
       products.visible,
     );
 
-    const loadMoreBtn = document.querySelector(`[data-loadmore=${e.target.dataset.btnreplace}]`);
+    const loadMoreBtn = document.querySelector(
+      `[data-loadmore=${e.target.dataset.btnreplace}]`,
+    );
     loadMoreBtn.addEventListener('click', loadMoreProducts);
     loadMoreBtn.classList.remove('hidden');
 
@@ -96,15 +95,18 @@ export async function getCategoryListItem(category) {
       endOfCategoryHandler(categoryItems, loadMoreBtn, products);
     }
 
-    const seeAllButton = document.querySelector(`[data-btnReplace=${category}]`);
+    const seeAllButton = document.querySelector(
+      `[data-btnReplace=${category}]`,
+    );
     seeAllButton.textContent = 'See less';
     seeAllButton.addEventListener('click', seeLessProducts);
-
 
     function seeLessProducts() {
       loadMoreBtn.classList.add('hidden');
       getSlider();
-      const seeAllButton = document.querySelector(`[data-btnReplace=${category}]`);
+      const seeAllButton = document.querySelector(
+        `[data-btnReplace=${category}]`,
+      );
       seeAllButton.textContent = 'See all';
       seeAllButton.addEventListener('click', seeAllProducts);
       seeAllButton.removeEventListener('click', seeLessProducts);
@@ -115,8 +117,8 @@ export async function getCategoryListItem(category) {
 function innerMarkup(categoryItems, start, end) {
   return `
   ${categoryItems.slice(start, end).reduce((acc, item) => {
-      return (acc += getItemMarkup(item));
-    }, '')}`;
+    return (acc += getItemMarkup(item));
+  }, '')}`;
 }
 
 function endOfCategoryHandler(categoryItems, loadMoreBtn, products) {
