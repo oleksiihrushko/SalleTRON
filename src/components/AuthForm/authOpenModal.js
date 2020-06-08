42dfc77aa284c564b4c313fe49e960e54ade1960
Tureckiy <ev.zhdanov@gmail.com>
2020-06-08 16:17:20 +0300

    adding login btn actionv02

---
+++ src/components/AuthForm/authOpenModal.js
@@ -0,0 +1,24 @@
+import openModal from './forOpenModal';
+
+import * as basicLightbox from 'basiclightbox';
+import '../../../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import authFormLogic from './authFormLogic';
+
+// const closeModalBtn = document.querySelector(
+//   'button[data-action="close-modal"]',
+// );
+
+const showModal = () => {
+  const instance = basicLightbox.create(
+    `<div>
+    <div class="modal">${openModal()}</div>
+    </div>`,
+  );
+
+  instance.show();
+  authFormLogic();
+};
+
+// closeModalBtn.addEventListener('click', showModal);
+
+export default showModal;
