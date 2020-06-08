46d3d6ec6f81225df475f7ee1dd04b7104edeab0
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-06-08 23:44:25 +0300

    fixed desctop and tablet

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -12,35 +12,42 @@ import * as basicLightbox from 'basiclightbox'
 // const modal = document.querySelector('.modal');
 const openItemModal = (id) => {
 
-apiService.getProductById(id).then(data => {
-  const glide = new Glide('.glide', {
-    type: 'carousel',
-    perView: 1,
-    dots: '#dots',
-  });
-  // modal.innerHTML = ItemModalHbs(data);
+  apiService.getProductById(id).then(data => {
+    const glide = new Glide('.glide', {
+      type: 'carousel',
+      perView: 1,
+      dots: '#dots',
+    });
+    // modal.innerHTML = ItemModalHbs(data);
 
-const instance = basicLightbox.create(`
+    const instance = basicLightbox.create(`
     <div>${ItemModalHbs(data)}</div>
 `, {
-  closable: false
-})
+      closable: false
+    })
 
-instance.show()
-const closeItemModalBtn = document.querySelector('.btnClose')
-closeItemModalBtn.addEventListener('click', ()=>instance.close());
+    instance.show()
+    const closeItemModalBtn = document.querySelector('.btnClose')
+    closeItemModalBtn.addEventListener('click', () => instance.close());
 
 
 
-  glide.mount();
-  const tabletItemPicture = document.querySelector('.tablet_picture');
-  const sMarkup = `
+    glide.mount();
+    const tabletItemPicture = document.querySelector('.tablet_picture');
+    const sMarkup = `
   <div class="itemModal__MainImageWrapper">
       <img src="${data.images[0]}" alt="" class="itemModal__MainImage">
   </div> 
   `
-  tabletItemPicture.insertAdjacentHTML("beforeend", sMarkup)
-  tabletItemPicture.insertAdjacentHTML("beforeend", tabletItemPictureMarkup(data));
-});
+    tabletItemPicture.insertAdjacentHTML("beforeend", sMarkup)
+    tabletItemPicture.insertAdjacentHTML("beforeend", tabletItemPictureMarkup(data));
+    const clickedPicture = document.querySelectorAll('.itemSliderPicture');
+    const itemModal__MainImage = document.querySelector('.itemModal__MainImage')
+    clickedPicture.forEach(picture => {
+      picture.addEventListener("click", (e) => {
+        itemModal__MainImage.setAttribute("src", e.target.getAttribute("src"));
+      })
+    })
+  });
 }
 export default openItemModal;
