dc5d9db396bf185a30af59aafee7007ac8e56a25
amie-affection <amie.affection@gmail.com>
2020-06-08 16:41:29 +0300

    init AuthForm

--- src/components/AuthForm/authOpenModal.js
+++ src/components/AuthForm/authOpenModal.js
@@ -5,26 +5,26 @@ import * as basicLightbox from 'basiclightbox';
 import '../../../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import authFormLogic from './authFormLogic';
 
-const loginModal = document.querySelector('.login-btn');
-const registrationModal = document.querySelector('.form__register-btn');
-registrationModal.addEventListener('click', showModal);
-loginModal.addEventListener('click', showModal);
-
-// const closeModalBtn = document.querySelector(
-//   'button[data-action="close-modal"]',
-// );
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-modal"]',
+);
 
 const showModal = () => {
   const instance = basicLightbox.create(
     `<div>
-    <div class="modal">${openModal()}</div>
-    </div>`,
+      <div class="modal">${openModal()}</div>
+      </div>`,
   );
 
   instance.show();
   authFormLogic();
 };
 
-// closeModalBtn.addEventListener('click', showModal);
+closeModalBtn.addEventListener('click', showModal);
+
+const loginModal = document.querySelector('.login-btn');
+const registrationModal = document.querySelector('.form__register-btn');
+registrationModal.addEventListener('click', showModal);
+loginModal.addEventListener('click', showModal);
 
 export default showModal;
