7c086e45203e4701db3dcfa3e589c04edf9c02ec
hrushkooleksii <hrushkooleksii@gmail.com>
2020-06-08 18:37:49 +0300

    fixing index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import './css/styles.scss';
-import './components/mainPage/mainPage';
-<<<<<<< HEAD
-import './components/ItemModal/ItemModal';
-import './components/navigationMenu/navigationMenu';
 import './services/api';
 import './components/header/header';
-
-import './components/authentication/authServises'
-<<<<<<< HEAD
-=======
-
+import './components/navigationMenu/navigationMenu';
+import './components/ItemModal/ItemModal';
+import './components/authentication/authServises';
 import './components/AuthForm/authOpenModal';
->>>>>>> AuthForm
-=======
-
-import './components/AuthForm/authOpenModal';
->>>>>>> authentication
