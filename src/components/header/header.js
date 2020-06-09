9bd1d8efd5dc1d8f428cfc6d5bac33dc969d9197
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-06-09 10:08:06 +0300

    search

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,14 @@
 import './header.scss';
-// import apiServices from '../../services/api';
+import apiServices from '../../services/api';
 
 const hamburger = document.querySelector('.hamburger');
 const burgerMenu = document.querySelector('.header__burgerMenu');
 const filterBtn = document.querySelector('.buttonFilter');
+const searchBar = document.querySelector('.header__search');
 hamburger.addEventListener('click', toggleBurger);
 filterBtn.addEventListener('click', toggleTabletFilter);
+searchBar.addEventListener('input', searchData);
+// searchBar.addEventListener('input', searchCategory);
 
 function toggleBurger(e) {
   e.preventDefault();
@@ -24,3 +27,15 @@ function toggleTabletFilter(e) {
     burgerMenu.classList.toggle('filterIsActiveForTablet');
   }
 }
+
+function searchData(e) {
+  const inputValue = e.currentTarget.value;
+
+  apiServices.getProducts().then(data => {
+    const productData = data.filter(product => console.log(product));
+  });
+}
+
+// function searchCategory(e) {
+//   apiServices.getProductsByCategory().then(res => console.log(res));
+// }
