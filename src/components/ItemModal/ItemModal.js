905bbe6462aaca7a8851a4d4ae667c754858bc61
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-06-09 19:41:10 +0300

    fixed favorites

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -13,6 +13,12 @@ import * as basicLightbox from 'basiclightbox'
 //   password:'111111'
 // })
 
+// apiService.signInUser({
+//   email: 'master-321@i.ua',
+//   password: '111111'
+// })
+
+
 // const modal = document.querySelector('.modal');
 const openItemModal = (id) => {
 
@@ -33,8 +39,8 @@ const openItemModal = (id) => {
     instance.show()
 
     const favoriteBtn = document.querySelector('.modal__share-item-heart-cvg');
-    console.log(localStorage.getItem('user').favorites);
-    if (localStorage.getItem('user').favorites.includes(id)) {
+    // console.log(JSON.parse(localStorage.getItem('user')));
+    if (JSON.parse(localStorage.getItem('user')).favorites.includes(id)) {
       favoriteBtn.innerHTML = `
   <path d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
   C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/>
@@ -48,12 +54,12 @@ const openItemModal = (id) => {
     backItemModalBtn.addEventListener('click', () => instance.close());
 
     favoriteBtn.addEventListener('click', (e) => {
-      if (!localStorage.getItem('user').favorites.includes(id)) {
+      if (!JSON.parse(localStorage.getItem('user')).favorites.includes(id)) {
 
         // apiService.addUserFavorite(id);fill="red"
-        localStorage.setItem('user', JSON.stringify({
-          favorites: [id],
-        }))
+        // localStorage.setItem('user', JSON.stringify({
+        //   favorites: [id],
+        // }))
         e.target.innerHTML = `
       <path d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
 			C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/>
