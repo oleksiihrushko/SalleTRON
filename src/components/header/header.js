123a666e48a83930785910dc21b590c20c88d648
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-06-09 11:47:21 +0300

    header reg button removed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ const searchBar = document.querySelector('.header__search');
 const mainPage = document.querySelector('.mainPage');
 hamburger.addEventListener('click', toggleBurger);
 filterBtn.addEventListener('click', toggleTabletFilter);
-searchBar.addEventListener('input', search__Data);
+searchBar.addEventListener('input', searchData);
 // searchBar.addEventListener('input', searchCategory);
 function toggleBurger(e) {
   e.preventDefault();
@@ -25,23 +25,40 @@ function toggleTabletFilter(e) {
     burgerMenu.classList.toggle('filterIsActiveForTablet');
   }
 }
+
 function searchData(e) {
   const inputValue = e.currentTarget.value;
-  if (inputValue === '') {
-    mainPage.innerHTML = `
-      <aside class="advBlock"></aside>
-      <section class="categoryList"></section>
-      <button class="button renderBtn"><span>More categories</span></button>
-      `;
-    //! сделать функцию перезаписи страницы вызвав функции Жени и Мишы
-  }
-  apiServices.getProducts().then(data => {
-    const productData = data.filter(product =>
-      product.name.toLowerCase().includes(inputValue.toLowerCase()),
-    );
-    console.log(productData);
+  if (inputValue === '') return;
+  // {
+  //   mainPage.innerHTML = `
+  //     <aside class="advBlock"></aside>
+  //     <section class="categoryList"></section>
+  //     <button class="button renderBtn"><span>More categories</span></button>
+  //     `;
+  //   //! сделать функцию перезаписи страницы вызвав функции Жени и Мишы
+  // }
+
+  apiServices.getCategoriesList().then(data => {
+    data.forEach(item => {
+      if (item.toLowerCase().includes(inputValue.toLowerCase())) {
+        if (inputValue.length >= 3) {
+          console.log('ok');
+        }
+        // else {
+        //   console.log('no result');
+        // }
+        return;
+      }
+    });
   });
+  // apiServices.getProducts().then(data => {
+  //   const productData = data.filter(product =>
+  //     product.name.toLowerCase().includes(inputValue.toLowerCase()),
+  //   );
+  //   console.log(productData);
+  // });
 }
+
 // function searchCategory(e) {
 //   apiServices.getProductsByCategory().then(res => console.log(res));
 // }
