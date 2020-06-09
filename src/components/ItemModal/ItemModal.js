39782e3249d3522344f0166bcbda69dd087e6e56
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-06-09 13:22:00 +0300

    fixed mobile

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -13,7 +13,7 @@ import * as basicLightbox from 'basiclightbox'
 const openItemModal = (id) => {
 
   apiService.getProductById(id).then(data => {
-    const glide = new Glide('.glide', {
+    const glide = new Glide('.glideItemModal', {
       type: 'carousel',
       perView: 1,
       dots: '#dots',
@@ -27,12 +27,45 @@ const openItemModal = (id) => {
     })
 
     instance.show()
+
+    const favoriteBtn = document.querySelector('.modal__share-item-heart-cvg');
+//     if (localStorage.getItem('user').favorites.includes(id)) {
+//       favoriteBtn.innerHTML = `
+//   <path d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
+//   C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/>
+// `;
+//     }
+
     const closeItemModalBtn = document.querySelector('.btnClose')
     closeItemModalBtn.addEventListener('click', () => instance.close());
 
+    const backItemModalBtn = document.querySelector('.btnBack')
+    backItemModalBtn.addEventListener('click', () => instance.close());
 
+    favoriteBtn.addEventListener('click', (e) => {
+//       if (!localStorage.getItem('user').favorites.includes(id)) {
+
+//         // apiService.addUserFavorite(id);
+//         localStorage.setItem('user', JSON.stringify({
+//           favorites: [id],
+//         }))
+//         e.target.innerHTML = `
+//       <path d="M255,489.6l-35.7-35.7C86.7,336.6,0,257.55,0,160.65C0,81.6,61.2,20.4,140.25,20.4c43.35,0,86.7,20.4,114.75,53.55
+// 			C283.05,40.8,326.4,20.4,369.75,20.4C448.8,20.4,510,81.6,510,160.65c0,96.9-86.7,175.95-219.3,293.25L255,489.6z"/>
+// `;
+//         console.log(id);
+//       } else {
+//         localStorage.setItem('user', JSON.stringify({
+//           favorites: [id],
+//         }))
+
+//       }
+
+console.log(localStorage.getItem('user').favorites);
+    })
 
     glide.mount();
+
     const tabletItemPicture = document.querySelector('.tablet_picture');
     const sMarkup = `
   <div class="itemModal__MainImageWrapper">
