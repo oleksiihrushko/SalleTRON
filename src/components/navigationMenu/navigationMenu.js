d74ec38a951ac50b2cec6b49496f728ac32c65f6
Pedko777 <yuriipedko@gmail.com>
2020-06-09 18:39:13 +0300

    Add  filter by category

--- src/components/navigationMenu/navigationMenu.js
+++ src/components/navigationMenu/navigationMenu.js
@@ -2,34 +2,34 @@ import './navigationMenu.scss';
 import navigationCard from './navigation.hbs';
 import mobileNavigationCard from './mobileNavigation.hbs';
 import api from '../../services/api.js';
-import {paginationCategore} from "../categoryListItem/renderCategoryList"
-// import {seeAllProducts } from '../categoryListItem/categoryListItem'
-
+import { paginationCategore } from '../categoryListItem/renderCategoryList';
+import searchBarHbs from '../header/searchBar.hbs'
 
 const navigationFilter = document.querySelector('.header__burgerMenu');
+const categoryList = document.querySelector('.categoryList');
 
 //=========== render list item
 let size = '';
 
 let categories = [];
-window.addEventListener('resize', () => {
-  const insert = categoriesArr => {
-    let markup = '';
-    if (size === 'tablet' && window.matchMedia('(max-width: 767px)').matches) {
-      markup = mobileNavigationCard(categoriesArr);
-      navigationFilter.innerHTML = markup;
-      size = 'mobile';
-    } else if (
-      size === 'mobile' &&
-      !window.matchMedia('(max-width: 767px)').matches
-    ) {
-      markup = navigationCard(categoriesArr);
-      navigationFilter.innerHTML = markup;
-      size = 'tablet';
-    }
-  };
-  insert(categories);
-});
+// window.addEventListener('resize', () => {
+//   const insert = categoriesArr => {
+//     let markup = '';
+//     if (size === 'tablet' && window.matchMedia('(max-width: 767px)').matches) {
+//       markup = mobileNavigationCard(categoriesArr);
+//       navigationFilter.innerHTML = markup;
+//       size = 'mobile';
+//     } else if (
+//       size === 'mobile' &&
+//       !window.matchMedia('(max-width: 767px)').matches
+//     ) {
+//       markup = navigationCard(categoriesArr);
+//       navigationFilter.innerHTML = markup;
+//       size = 'tablet';
+//     }
+//   };
+//   insert(categories);
+// });
 
 const insert = categoriesArr => {
   let markup = '';
@@ -40,42 +40,30 @@ const insert = categoriesArr => {
     size = 'tablet';
     markup = navigationCard(categoriesArr);
   }
-  // navigationFilter.insertAdjacentHTML('afterbegin', markup);
+
   navigationFilter.innerHTML = markup;
 };
 api.getCategoriesList().then(data => {
   insert(data);
   categories = [...data];
+
+  const navigationFilterList = document.querySelector('.navigationFilterList');
+  navigationFilterList.addEventListener('click', e => {
+    if (e.target.nodeName !== 'LI') return;
+    categoryList.innerHTML = "";
+    api.getProductsByCategory(e.target.innerHTML).then(res => {
+      console.log(res);
+      categoryList.insertAdjacentHTML('beforeend', searchBarHbs(res));
+    });
+  });
 });
 
-//=============== openByCategory
-
-
-const navigationFilterList = document.querySelector('.navigationFilterList');
-navigationFilterList.addEventListener('click', e => {
-  if (e.target.nodeName !== 'LI') return;
-  
-  // const eObj = {
-  //   target: {
-  //     dataset: {
-  //       btnreplace: e.target.value,
-  //     },
-  //   },
-  // };
-
-  seeAllProductsByFilter(e.target.value);
-});
 
 //*================== reset
 
 const navigationButtonRestore = document.querySelector('.buttonRestore');
 navigationButtonRestore.addEventListener('click', e => {
   if (e.target.nodeName !== 'BUTTON') return;
-  api.getCategoriesList().then(data => {
-    insert(data);
-    categories = [...data];
-  });
-  //* pagination
-  paginationCategore(2)
-  //*innerHTML
+  categoryList.innerHTML = '';
+  paginationCategore(2);
 });
