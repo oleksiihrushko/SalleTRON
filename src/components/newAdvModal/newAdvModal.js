e85d32634a13c428545cabd4c2c08cc80b48688e
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-06-10 11:38:59 +0300

    finish

--- src/components/newAdvModal/newAdvModal.js
+++ src/components/newAdvModal/newAdvModal.js
@@ -7,7 +7,8 @@ btnOpenModel.addEventListener("click", newOpenModel);
 function newOpenModel () {
   
 const instance = basicLightbox.create(
-  `<div class="new-adv-modal-div">
+  `<button class=new-adv-modal-close-btn></button>
+  <div class="new-adv-modal-div">
   <form form action="#" class="new-adv-modal-form" method="post">
 
         <h2 class="modal-form-title">Create new advertisement</h2> 
@@ -186,59 +187,17 @@ function checkValue (e) {
     submitBtn.img4 = img4.getAttribute("src");
     submitBtn.img5 = img5.getAttribute("src");
     submitBtn.img6 = img6.getAttribute("src");
+    
  }
+
+ const closeBtn = document.querySelector('.new-adv-modal-close-btn');
+  closeBtn.addEventListener('click', closeModalHandler);
+  window.addEventListener('keydown', closeModalHandler);
+
+  function closeModalHandler(e) {
+    (e.code === 'Escape' || e.target === closeBtn) && instance.close();
+
+    window.removeEventListener('keydown', closeModalHandler);
+    closeBtn.removeEventListener('click', closeModalHandler);
+  }
 }
-
-
-// let imgMarkup = '';
-// for (let i = 1; i < 7; i += 1 ) {
-// imgMarkup += `
-// <li>
-//   <input type="file"name="name" class="off-text-input" id="idImg-${i}" data-id="${i}">
-//   <label data-labelID="${i}" for="idImg-${i}" class="new-adv-modal-form-addimg${i}  new-adv-modal-form-addimg new-adv-modal-form-diseibl-img">
-//     <img class="import-img import-img-${i}" src="#" alt="" width="75" height="60">
-//   </label>
-// </li>`;
-
-// }
-
-// document.querySelector(".ul-img-lable").insertAdjacentHTML('beforeend',imgMarkup)
-// let img;      
-// const listImg = document.querySelector(".ul-img-lable");
-// listImg.addEventListener("click", newLi);
-// function newLi (e) {
-// if (e.target.nodeName === "LABEL") {
-//   const labelID = e.target.dataset.labelid;
-//   img = document.querySelector(`.import-img-${labelID}`);
-//   const input= document.querySelector(`#idImg-${labelID}`);
-//   input.addEventListener("change", nextInput);
-//   function nextInput () {
-//     const file = input.files[0],
-//     reader = new FileReader();
-  
-//       reader.onload = function () {
-//         const b64 = reader.result;
-//         img.src = b64;
-//       };
-//       if (file) {
-//         reader.readAsDataURL(file);
-//       } else {
-//         preview.src = "";
-//       }
-//       input.removeEventListener("change", nextInput); 
-//   }
-//  }
-// }
-
-// const prodNameOff = document.querySelector(".new-adv-modal-form-name");
-// const prodDescriptOff = document.querySelector(".new-adv-modal-form-product-descriptions");
-// const placeholderOFF = () => {
-//     if (window.innerWidth > 768) {
-//         prodNameOff.setAttribute("placeholder", "");
-//         prodDescriptOff.setAttribute("placeholder", "");
-//         return;
-//       }
-// }
-// placeholderOFF();
-
-//========================================================================================================
